585d7519b0ce304de8c77580c9ec70bd23a44302
Army-py <61599555+Army-py@users.noreply.github.com>
2024-04-13 19:13:52 +0200

    Implement Config

---
+++ src/main/java/fr/army/fluix/config/Config.java
@@ -0,0 +1,38 @@
+package fr.army.fluix.config;
+
+import fr.army.fluix.chat.FluixChat;
+import net.md_5.bungee.config.Configuration;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Config {
+
+    private final Configuration config;
+
+    public static Map<String, FluixChat> chats;
+
+    public Config(Configuration config) {
+        this.config = config;
+    }
+
+    public void load() {
+        chats = loadChats();
+    }
+
+    private Map<String, FluixChat> loadChats() {
+        Configuration chatSection = config.getSection("chats");
+        return chatSection.getKeys().stream()
+                .collect(Collectors.toMap(
+                                key -> key,
+                                key -> new FluixChat.Builder()
+                                        .setCommand(chatSection.getString(key + ".command"))
+                                        .setPrefix(chatSection.getString(key + ".prefix"))
+                                        .setFormat(chatSection.getString(key + ".format"))
+                                        .setWritePermission(chatSection.getString(key + ".permission.write"))
+                                        .setReadPermission(chatSection.getString(key + ".permission.read"))
+                                        .build()
+                        )
+                );
+    }
+}
