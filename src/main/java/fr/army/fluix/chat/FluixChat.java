149b0dfb81ff2139ee39c5d08189467fac8eb477
Army-py <61599555+Army-py@users.noreply.github.com>
2024-04-13 19:19:17 +0200

    Add command aliases

--- src/main/java/fr/army/fluix/chat/FluixChat.java
+++ src/main/java/fr/army/fluix/chat/FluixChat.java
@@ -2,11 +2,13 @@ package fr.army.fluix.chat;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Objects;
 
 public class FluixChat {
 
     private final String command;
+    private final List<String> aliases;
     private final String prefix;
     private final String format;
     private final String write_permission;
@@ -14,11 +16,13 @@ public class FluixChat {
 
     private FluixChat(
             @NotNull String command,
+            @NotNull List<String> aliases,
             @NotNull String prefix,
             @NotNull String format,
             @NotNull String write_permission,
             @NotNull String read_permission) {
         this.command = Objects.requireNonNull(command, "command");
+        this.aliases = Objects.requireNonNull(aliases, "aliases");
         this.prefix = Objects.requireNonNull(prefix, "prefix");
         this.format = Objects.requireNonNull(format, "format");
         this.write_permission = Objects.requireNonNull(write_permission, "write_permission");
@@ -27,6 +31,10 @@ public class FluixChat {
 
     public String getCommand() {
         return command;
+    }
+
+    public List<String> getAliases() {
+        return aliases;
     }
 
     public String getPrefix() {
@@ -48,6 +56,7 @@ public class FluixChat {
 
     public static class Builder {
         private String command;
+        private List<String> aliases;
         private String prefix;
         private String format;
         private String write_permission;
@@ -55,6 +64,11 @@ public class FluixChat {
 
         public Builder setCommand(@NotNull String command) {
             this.command = command;
+            return this;
+        }
+
+        public Builder setAliases(@NotNull List<String> aliases) {
+            this.aliases = aliases;
             return this;
         }
 
@@ -79,7 +93,7 @@ public class FluixChat {
         }
 
         public FluixChat build() {
-            return new FluixChat(command, prefix, format, write_permission, read_permission);
+            return new FluixChat(command, aliases, prefix, format, write_permission, read_permission);
         }
     }
 }
